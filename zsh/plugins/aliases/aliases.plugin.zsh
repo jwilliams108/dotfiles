554b75bf8bb721ff1effcf956dd20f407ba5a570
jwilliams108 <jwilliams108@users.noreply.github.com>
2015-02-02 11:35:53 +1100

    updating zsh config

--- zsh/plugins/aliases/aliases.plugin.zsh
+++ zsh/plugins/aliases/aliases.plugin.zsh
@@ -1,3 +1,11 @@
+#misc
+alias pwdcp="pwd | tr -d '\n' | pbcopy"
+alias h='history | grep'
+
+#chrome
+alias canary='/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary'
+alias chrome='/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
+
 #homebrew
 alias bi='brew -v install'
 alias bu='brew -v upgrade'
@@ -9,3 +17,11 @@ alias tmux='tmux -u'
 
 #macvim
 alias mtab='mvim -p --remote-tab'
+
+#fasd
+alias j='z'
+alias m='d -e mvim'
+
+#js/phpcs
+alias jscs='jscs -p airbnb'
+alias phpcs="phpcs --standard=$HOME/.phpcs/phpcs.xml"
