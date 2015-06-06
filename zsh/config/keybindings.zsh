d227da3b9441610e118da2bea0ea1cb15b66e9ad
jwilliams108 <jwilliams108@users.noreply.github.com>
2015-06-06 23:24:29 +1000

    cleaning up keybindings

--- zsh/config/keybindings.zsh
+++ zsh/config/keybindings.zsh
@@ -1,17 +1,19 @@
-# vi style incremental search
+# incremental search
 bindkey '^R' history-incremental-search-backward
 bindkey '^S' history-incremental-search-forward
-bindkey '^P' history-search-backward
-bindkey '^N' history-search-forward
-bindkey "^[[A" history-beginning-search-backward
-bindkey "^[[B" history-beginning-search-forward
 
-# command line movement
+# history search
+#bindkey "^[[A" history-beginning-search-backward
+#bindkey "^[[B" history-beginning-search-forward
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+
+# emacs like command line movement
 bindkey "^A" beginning-of-line
 bindkey "^E" end-of-line
 bindkey "^W" backward-delete-word
-bindkey "^b" backward-word
-bindkey "^f" forward-word
-bindkey "^d" delete-word
-bindkey "^k" kill-line
+bindkey "^B" backward-word
+bindkey "^F" forward-word
+bindkey "^D" delete-word
+bindkey "^K" kill-line
 #bindkey "${key[Delete]}" delete-char
