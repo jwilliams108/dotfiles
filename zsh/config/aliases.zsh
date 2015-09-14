09f6c1cc68a0db3c7fa7687531f35f2c71fb8eb0
jwilliams108 <jwilliams108@users.noreply.github.com>
2015-09-14 16:40:23 +1000

    adding vhosts + help alias and cleanup

--- zsh/config/aliases.zsh
+++ zsh/config/aliases.zsh
@@ -1,28 +1,34 @@
-#misc
+# misc
+alias vhosts="vi /etc/hosts"
 alias pwdcp="pwd | tr -d '\n' | pbcopy"
 alias h='history | grep'
 
-#chrome
+# chrome
 alias canary='/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary'
 alias chrome='/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
 
-#homebrew
+# homebrew
 alias bi='brew -v install'
 alias bu='brew -v upgrade --all'
 alias bup='brew update'
 alias bcup='brew cleanup'
 alias bout='brew outdated'
 
-#tmux
+# tmux
 alias tmux='tmux -u'
 
 #macvim
 alias mtab='mvim -p --remote-tab'
 
-#fasd
+# fasd
 alias j='z'
 alias m='d -e mvim'
 
-#js/phpcs
+# js/phpcs
 alias jscs='jscs -p airbnb'
 alias phpcs="phpcs --standard=$HOME/.phpcs/phpcs.xml"
+
+# help
+unalias run-help
+autoload run-help
+HELPDIR=/usr/local/share/zsh/help
