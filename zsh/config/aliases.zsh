6a251f2af3b1c5f8e583b942080c453ec8daab26
jwilliams108 <jwilliams108@hotmail.com>
2016-04-02 15:50:50 +1100

    cleaning up zsh aliases

--- zsh/config/aliases.zsh
+++ zsh/config/aliases.zsh
@@ -1,3 +1,6 @@
+# coreutils
+alias ls=ls -N --color=tty
+
 # misc
 alias vhosts='vi /etc/hosts'
 alias pwdcp="pwd | tr -d '\n' | pbcopy"
@@ -5,10 +8,6 @@ alias h='history | grep'
 
 # cmus
 alias cmus='cmus 2>/dev/null'
-
-# chrome
-alias canary='/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary'
-alias chrome='/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
 
 # homebrew
 alias bi='brew -v install'
@@ -26,10 +25,6 @@ alias mtab='mvim -p --remote-tab'
 # fasd
 alias j='z'
 alias m='d -e mvim'
-
-# js/phpcs
-alias jscs='jscs -p airbnb'
-alias phpcs="phpcs --standard=$HOME/.phpcs/phpcs.xml"
 
 # help
 unalias run-help
