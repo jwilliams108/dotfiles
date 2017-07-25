397e723b532fec47d1ab1d899df15d17f12cdf9c
jwilliams108 <jwilliams108@hotmail.com>
2017-07-25 11:20:15 +1000

    adding inspect alias in preference to inspect-node

--- zsh/config/aliases.zsh
+++ zsh/config/aliases.zsh
@@ -35,3 +35,4 @@ HELPDIR=/usr/local/share/zsh/help
 
 # dev
 alias gentags='find . -type f -iregex ".*\.js$" -not -path "./node_modules/*" -exec jsctags {} -f \; | sed '/^$/d' | sort'
+alias inspect='node --inspect-brk'
