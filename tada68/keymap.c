3d62b7045370bd6c24ef7c0c99cb62d913fff1de
jwilliams108 <jwilliams108@hotmail.com>
2017-11-23 10:53:04 +1100

    switch over to "new" macro method for custom keycodes

--- tada68/keymap.c
+++ tada68/keymap.c
@@ -1,25 +1,29 @@
 #include "tada68.h"
 #include "timer.h"
 
-#define TAPPING_TERM 165
-#define _______ KC_TRNS
-
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
-#define _BL 0
-#define _FL 1
+// Layer definitions for readability
+enum custom_layers {
+  _BL,
+  _FL
+};
 
 // special keys
 #define CTRLESC MT(MOD_LCTL, KC_ESC)
 #define HYPER   LALT(LGUI(KC_LCTL))
 
-// tap/hold macros
-#define LSFT_PAREN 0
-#define RSFT_PAREN 1
-#define LGUI_BRACE 2
-#define RGUI_BRACE 3
+enum custom_keycodes {
+  LSFT_PN = SAFE_RANGE,
+  RSFT_PN,
+  LGUI_BR,
+  RGUI_BR
+};
+
+// Length of time before a tap is considered a hold
+#define TAPPING_TERM 165
+
+// Fillers to make layering clear
+#define _______ KC_TRNS
+#define XXXXXXX KC_NO
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap _BL: (Base Layer) Default Layer
@@ -35,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |Hypr|Alt |Cmd |        Space          |Cmd| FN|Ctrl|Lef|Dow|Rig |
    * `----------------------------------------------------------------'
    *
-   *  Ctrl <-- tap for escape
+   *  Ctrl <-- tap for escape (left only)
    *   Cmd <-- tap for braces
    * Shift <-- tap for parenthesis
    */
@@ -43,8 +47,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,  KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,     KC_9,   KC_0,KC_MINS,  KC_EQL, KC_BSPC, KC_GRV, \
   KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,     KC_O,   KC_P,KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL, \
  CTRLESC,  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,     KC_L,KC_SCLN,KC_QUOT,           KC_ENT,KC_PGUP, \
-    M(0),          KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,KC_SLSH,    M(1),   KC_UP,KC_PGDN, \
-   HYPER, KC_LALT,    M(2),                KC_SPC,                            M(3),MO(_FL),KC_RCTRL,KC_LEFT,KC_DOWN,KC_RGHT),
+ LSFT_PN,          KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,KC_SLSH, RSFT_PN,   KC_UP,KC_PGDN, \
+   HYPER, KC_LALT, LGUI_BR,                KC_SPC,                        RGUI_BR,MO(_FL),KC_RCTRL,KC_LEFT, KC_DOWN,KC_RGHT),
 
   /* Keymap _FL: Function Layer
    * ,----------------------------------------------------------------.
@@ -67,58 +71,54 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______,_______,_______,                _______,                        _______,_______,_______,_______,_______,_______),
 };
 
-static uint16_t start[4];
+static uint16_t key_timer[4]; // this must match the length of the custom_keycodes enum
 
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
-  switch(id) {
-    case LSFT_PAREN:
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case LSFT_PN:
       if (record->event.pressed) {
-        start[LSFT_PAREN] = timer_read();
-        return MACRO(D(LSFT), END);
+        key_timer[0] = timer_read();
+        register_code(KC_LSFT);
       } else {
-        if (timer_elapsed(start[LSFT_PAREN]) > TAPPING_TERM) {
-          return MACRO(U(LSFT), END);
-        } else {
-          return MACRO(D(LSFT), T(9), U(LSFT), END);
+        unregister_code(KC_LSFT);
+        if (timer_elapsed(key_timer[0]) < TAPPING_TERM) {
+          SEND_STRING("(");
         }
       }
-      break;
-    case RSFT_PAREN:
+      return false; break;
+    case RSFT_PN:
       if (record->event.pressed) {
-        start[RSFT_PAREN] = timer_read();
-        return MACRO(D(RSFT), END);
+        key_timer[1] = timer_read();
+        register_code(KC_RSFT);
       } else {
-        if (timer_elapsed(start[RSFT_PAREN]) > TAPPING_TERM) {
-          return MACRO(U(RSFT), END);
-        } else {
-          return MACRO(D(RSFT), T(0), U(RSFT), END);
+        unregister_code(KC_RSFT);
+        if (timer_elapsed(key_timer[1]) < TAPPING_TERM) {
+          SEND_STRING(")");
         }
       }
-      break;
-    case LGUI_BRACE:
+      return false; break;
+    case LGUI_BR:
       if (record->event.pressed) {
-        start[LGUI_BRACE] = timer_read();
-        return MACRO(D(LGUI), END);
+        key_timer[2] = timer_read();
+        register_code(KC_LGUI);
       } else {
-        if (timer_elapsed(start[LGUI_BRACE]) > TAPPING_TERM) {
-          return MACRO(U(LGUI), END);
-        } else {
-          return MACRO(U(LGUI), D(LSFT), T(LBRACKET), U(LSFT), END);
+        unregister_code(KC_LGUI);
+        if (timer_elapsed(key_timer[2]) < TAPPING_TERM) {
+          SEND_STRING("{");
         }
       }
-      break;
-    case RGUI_BRACE:
+      return false; break;
+    case RGUI_BR:
       if (record->event.pressed) {
-        start[RGUI_BRACE] = timer_read();
-        return MACRO(D(RGUI), END);
+        key_timer[3] = timer_read();
+        register_code(KC_RGUI);
       } else {
-        if (timer_elapsed(start[RGUI_BRACE]) > TAPPING_TERM) {
-          return MACRO(U(RGUI), END);
-        } else {
-          return MACRO(U(RGUI), D(LSFT), T(RBRACKET), U(LSFT), END);
+        unregister_code(KC_RGUI);
+        if (timer_elapsed(key_timer[3]) < TAPPING_TERM) {
+          SEND_STRING("}");
         }
       }
-      break;
+      return false; break;
   }
-  return MACRO_NONE;
-}
+  return true;
+};
